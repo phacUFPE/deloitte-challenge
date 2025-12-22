6931b864c54e478e83f7548d7eaed5c10377e568
Pedro Cruz <phac@cin.ufpe.br>
2025-12-22 15:55:54 -0300

    feat: add use fetch

---
+++ frontend/src/composables/useFetch.ts
@@ -0,0 +1,49 @@
+import { ref, watch } from 'vue'
+import { AxiosError, type AxiosRequestConfig } from 'axios'
+
+import http from '@/api/http'
+
+export function useFetch<T>(
+  initialUrl: string,
+  initialParams: Record<string, string> = {},
+  config?: AxiosRequestConfig
+) {
+  const url = ref(initialUrl)
+  const params = ref({ ...initialParams })
+
+  const data = ref<T | null>(null)
+  const loading = ref(false)
+  const error = ref<string | null>(null)
+
+  const fetchData = async (overrideParams?: Record<string, string>) => {
+    loading.value = true
+    error.value = null
+
+    try {
+      const response = await http.get<T>(url.value, {
+        ...config,
+        params: overrideParams ? { ...params.value, ...overrideParams } : params.value,
+      })
+      data.value = response.data
+      return response.data
+    } catch (err: unknown) {
+      if (err instanceof AxiosError) {
+        error.value = err.message || 'An error occurred'
+      }
+      console.error(err)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  watch([url, params], () => fetchData(), { immediate: true, deep: true })
+
+  return {
+    data,
+    loading,
+    error,
+    url,
+    params,
+    fetchData,
+  }
+}
