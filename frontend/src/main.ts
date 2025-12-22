86e47687ad7cef6ec58b8625745883d36557d5b3
Pedro Cruz <phac@cin.ufpe.br>
2025-12-22 15:57:26 -0300

    chore: prettier format

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { vuetify } from "@/plugins/vuetify";
+import { vuetify } from '@/plugins/vuetify'
 
 import App from './App.vue'
 import router from '@/router'
