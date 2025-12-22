236b9c8655eef3c4aeaae909e551d957ae1aee60
Pedro Cruz <phac@cin.ufpe.br>
2025-12-22 15:55:13 -0300

    feat: add simple product card

---
+++ frontend/src/components/SimpleProductCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+import type { Product } from '@/types/Product'
+
+const props = defineProps<{
+  product: Product
+}>()
+
+const description = computed(() => props.product.description || 'No description')
+</script>
+
+<template>
+  <v-card height="200px">
+    <v-card-title>{{ props.product.title }}</v-card-title>
+    <v-card-text>
+      {{ description }}
+    </v-card-text>
+  </v-card>
+</template>
