30d8c24d7615537b02fca536831643fd9eaa232f
Pedro Cruz <phac@cin.ufpe.br>
2025-12-22 18:01:01 -0300

    chore: update vitest config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,14 +1,29 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+
+      '\\.(css|scss|sass)$': path.resolve(
+        __dirname,
+        'tests/__mocks__/styleMock.ts',
+      ),
+      'vuetify/styles': path.resolve(
+        __dirname,
+        'tests/__mocks__/styleMock.ts',
+      ),
+      'vuetify/lib/components': path.resolve(
+        __dirname,
+        'tests/__mocks__/styleMock.ts',
+      ),
     },
-  })
-)
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+  },
+})
