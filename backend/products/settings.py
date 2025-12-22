e19b8d9d547c35afd95ba94007f6f290d5b8b294
Pedro Cruz <phac@cin.ufpe.br>
2025-12-22 15:52:45 -0300

    chore: cors

--- backend/products/settings.py
+++ backend/products/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -46,7 +47,12 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
